34d950665d8f2733c529a0425c36e8bf01de8804
Diego <dasc2703@gmail.com>
2025-03-30 21:04:15 -0500

    App peliculas antes de ser desplegado

--- src/server.js
+++ src/server.js
@@ -1,46 +1,4 @@
-// Modulos //
-
-import express from "express";
-import movies from "./movies.json" assert { type: "json" };
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
-import validation from "./routes/validation.js";
-import filterMovies from "./filter/filter.js"
-
-// Server //
-
-const app = express();
+import app from './app.js'
 const PORT = 3000;
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-app.use(express.static(join(__dirname, "public")));
-app.set("view engine", "ejs");
-app.set("views", join(__dirname, "views"));
-
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "pages/inicio.html"));
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile(join(__dirname, "pages/login.html"));
-});
-
-app.get("/register", (req, res) => {
-  res.sendFile(join(__dirname, "pages/register.html"));
-});
-
-app.get("/movies", (req, res) => {
-  const filteredMovies = filterMovies(req.query, movies)
-
-  if (filteredMovies.length !== 0) {
-    let movies = filteredMovies;
-    return res.render("movies", { movies });
-  }
-
-  res.render("movies", { movies });
-});
-
-app.use(validation);
 
 app.listen(PORT, () => console.log(`Server on port http://localhost:${PORT}`));
